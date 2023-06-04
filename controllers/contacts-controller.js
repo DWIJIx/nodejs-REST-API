e218e3b1519f557c038cdf48220fc5f57b8d6847
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-06-04 12:47:15 +0300

    fix

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,19 +1,6 @@
-const Joi = require("joi");
 const contactsService = require("../models/contacts");
 const { HttpError } = require("../helpers");
 const { ctrlWrapper } = require("../decorators");
-
-const contactAddSchema = Joi.object({
-  name: Joi.string().alphanum().min(2).max(30).required().messages({
-    "any.required": "missing required name field",
-  }),
-  email: Joi.string().trim().email().required().messages({
-    "any.required": "missing required email field",
-  }),
-  phone: Joi.string().required().messages({
-    "any.required": "missing required phone field",
-  }),
-});
 
 const getAllContacts = async (req, res) => {
   const result = await contactsService.listContacts();
@@ -24,33 +11,21 @@ const getContactById = async (req, res) => {
   const { id } = req.params;
   const result = await contactsService.getContactById(id);
   if (!result) {
-    throw HttpError(404, `Movie with ${id} not found`);
+    throw HttpError(404, "Not found");
   }
   res.json(result);
 };
 
 const addContact = async (req, res) => {
-  const { error } = contactAddSchema.validate(req.body);
-  if (Object.keys(req.body).length === 0) {
-    throw HttpError(400, "missing fields");
-  } else if (error) {
-    throw HttpError(400, error.message);
-  }
   const result = await contactsService.addContact(req.body);
   res.status(201).json(result);
 };
 
 const updateContact = async (req, res) => {
-  const { error } = contactAddSchema.validate(req.body);
-  if (Object.keys(req.body).length === 0) {
-    throw HttpError(400, "missing fields");
-  } else if (error) {
-    throw HttpError(400, error.message);
-  }
   const { id } = req.params;
   const result = await contactsService.updateContact(id, req.body);
   if (!result) {
-    throw HttpError(404, `Movie with ${id} not found`);
+    throw HttpError(404, "Not found");
   }
   res.json(result);
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,14 +2,24 @@ const express = require("express");
 
 const router = express.Router();
 const contactsController = require("../../controllers/contacts-controller");
+const schemas = require("../../schemas/contacts-schemas");
+const { validateBody } = require("../../decorators");
 
 router.get("/", contactsController.getAllContacts);
 
 router.get("/:id", contactsController.getContactById);
 
-router.post("/", contactsController.addContact);
+router.post(
+  "/",
+  validateBody(schemas.contactAddSchema),
+  contactsController.addContact
+);
 
-router.put("/:id", contactsController.updateContact);
+router.put(
+  "/:id",
+  validateBody(schemas.contactAddSchema),
+  contactsController.updateContact
+);
 
 router.delete("/:id", contactsController.removeContact);
 
