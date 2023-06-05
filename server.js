03e0698db010a6331a048cef20358a59acd59c2e
DWIJIx <106489060+DWIJIx@users.noreply.github.com>
2023-06-05 13:26:01 +0300

    connect to database

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
 
 const app = require("./app");
-// const DB_HOST =
-//   "mongodb+srv://AndriiV:Vyd6snUMNePBlSSU@cluster0.olzdku9.mongodb.net/db-contacts?retryWrites=true&w=majority";
-const { DB_HOST } = process.env;
+
+const { DB_HOST, PORT = 3000 } = process.env;
 mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3000);
-    console.log("Server running. Use our API on port: 3000");
+    app.listen(PORT);
+    console.log(`Server running. Use our API on port: ${PORT}`);
   })
   .catch((error) => {
     console.log(error.message);
